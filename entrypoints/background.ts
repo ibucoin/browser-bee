a0db3a046b51983bf43691f16c3390481cf2db2d
ibucoin <ibucoin@gmail.com>
2025-12-19 14:14:27 +0800

    update

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,6 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
+
+  // 点击扩展图标时打开 sidepanel
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
